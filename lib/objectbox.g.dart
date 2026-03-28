ed6fb758698d75457cea5de66982ff1e05701c7e
Hasnain <patelhusnain02@gmail.com>
2026-03-28 15:16:17 +0530

    improvment in pos

--- lib/objectbox.g.dart
+++ lib/objectbox.g.dart
@@ -704,7 +704,7 @@ final _entities = <obx_int.ModelEntity>[
   obx_int.ModelEntity(
     id: const obx_int.IdUid(14, 5072473716951944501),
     name: 'EntityBill',
-    lastPropertyId: const obx_int.IdUid(19, 4104903407223595329),
+    lastPropertyId: const obx_int.IdUid(20, 6263340256701511849),
     flags: 0,
     properties: <obx_int.ModelProperty>[
       obx_int.ModelProperty(
@@ -820,6 +820,12 @@ final _entities = <obx_int.ModelEntity>[
         id: const obx_int.IdUid(19, 4104903407223595329),
         name: 'dueAmount',
         type: 8,
+        flags: 0,
+      ),
+      obx_int.ModelProperty(
+        id: const obx_int.IdUid(20, 6263340256701511849),
+        name: 'note',
+        type: 9,
         flags: 0,
       ),
     ],
@@ -2283,7 +2289,10 @@ obx_int.ModelDefinition getObjectBoxModel() {
         final utrNumberOffset = object.utrNumber == null
             ? null
             : fbb.writeString(object.utrNumber!);
-        fbb.startTable(20);
+        final noteOffset = object.note == null
+            ? null
+            : fbb.writeString(object.note!);
+        fbb.startTable(21);
         fbb.addInt64(0, object.id);
         fbb.addOffset(1, billNoOffset);
         fbb.addOffset(2, customerNameOffset);
@@ -2303,6 +2312,7 @@ obx_int.ModelDefinition getObjectBoxModel() {
         fbb.addFloat64(16, object.splitCash);
         fbb.addFloat64(17, object.splitOnline);
         fbb.addFloat64(18, object.dueAmount);
+        fbb.addOffset(19, noteOffset);
         fbb.finish(fbb.endTable());
         return object.id;
       },
@@ -2391,27 +2401,31 @@ obx_int.ModelDefinition getObjectBoxModel() {
           rootOffset,
           26,
         );
-        final object = EntityBill(
-          id: idParam,
-          billNo: billNoParam,
-          customerName: customerNameParam,
-          customerPhone: customerPhoneParam,
-          totalAmount: totalAmountParam,
-          discount: discountParam,
-          tax: taxParam,
-          grandTotal: grandTotalParam,
-          status: statusParam,
-          paymentMode: paymentModeParam,
-          amountReceived: amountReceivedParam,
-          changeReturned: changeReturnedParam,
-          utrNumber: utrNumberParam,
-          splitCash: splitCashParam,
-          splitOnline: splitOnlineParam,
-          dueAmount: dueAmountParam,
-          billDate: billDateParam,
-          createdAtUtcMs: createdAtUtcMsParam,
-          updatedAtUtcMs: updatedAtUtcMsParam,
-        );
+        final object =
+            EntityBill(
+                id: idParam,
+                billNo: billNoParam,
+                customerName: customerNameParam,
+                customerPhone: customerPhoneParam,
+                totalAmount: totalAmountParam,
+                discount: discountParam,
+                tax: taxParam,
+                grandTotal: grandTotalParam,
+                status: statusParam,
+                paymentMode: paymentModeParam,
+                amountReceived: amountReceivedParam,
+                changeReturned: changeReturnedParam,
+                utrNumber: utrNumberParam,
+                splitCash: splitCashParam,
+                splitOnline: splitOnlineParam,
+                dueAmount: dueAmountParam,
+                billDate: billDateParam,
+                createdAtUtcMs: createdAtUtcMsParam,
+                updatedAtUtcMs: updatedAtUtcMsParam,
+              )
+              ..note = const fb.StringReader(
+                asciiOptimization: true,
+              ).vTableGetNullable(buffer, rootOffset, 42);
         obx_int.InternalToManyAccess.setRelInfo<EntityBill>(
           object.items,
           store,
@@ -3561,6 +3575,11 @@ class EntityBill_ {
   /// See [EntityBill.dueAmount].
   static final dueAmount = obx.QueryDoubleProperty<EntityBill>(
     _entities[9].properties[18],
+  );
+
+  /// See [EntityBill.note].
+  static final note = obx.QueryStringProperty<EntityBill>(
+    _entities[9].properties[19],
   );
 
   /// see [EntityBill.items]
